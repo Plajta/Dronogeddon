03b782c358e420d5deb9cbf178add5e3dbe4a8b1
ENGO150 <v.smejkal06@gmail.com>
2022-11-04 14:18:27 +0100

    added some random funi things to video.py

--- src/video.py
+++ src/video.py
@@ -11,8 +11,6 @@ tello.streamon()
 frame_read = tello.get_frame_read()
 
 def videoRecorder():
-    # create a VideoWrite object, recoring to ./video.avi
-    # 创建一个VideoWrite对象，存储画面至./video.avi
     height, width, _ = frame_read.frame.shape
     video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
 
@@ -22,15 +20,16 @@ def videoRecorder():
 
     video.release()
 
-# we need to run the recorder in a seperate thread, otherwise blocking options
-#  would prevent frames from getting added to the video
-# 我们需要在另一个线程中记录画面视频文件，否则其他的阻塞操作会阻止画面记录
 recorder = Thread(target=videoRecorder)
 recorder.start()
 
 tello.takeoff()
 tello.move_up(100)
 tello.rotate_counter_clockwise(360)
+tello.flip_back();
+tello.flip_forward();
+tello.flip_left();
+tello.flip_right();
 tello.land()
 
 keepRecording = False
