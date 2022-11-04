d10f87e6d35865576427e5931ccb8e06fe47601c
ENGO150 <v.smejkal06@gmail.com>
2022-11-04 18:56:01 +0100

    changed gui code
    idk

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,23 +1,27 @@
-use glib::clone;
-use gtk::glib;
+use gtk4 as gtk;
 use gtk::prelude::*;
-
-fn on_activate(application: &gtk::Application)
+use gtk::
 {
-    let window = gtk::ApplicationWindow::new(application);
-    let button = gtk::Button::with_label("Hello World!");
-    button.connect_clicked(clone!(@weak window => move |_| window.close()));
-    window.set_child(Some(&button));
-    window.present();
-}
+    Application,
+    ApplicationWindow
+};
 
-fn main()
-{
-    let app = gtk::Application::builder()
-        .application_id("com.github.gtk-rs.examples.basic")
+fn main() {
+    let app = Application::builder()
+        .application_id("com.plajtacorp.dronogeddon")
         .build();
 
-    app.connect_activate(on_activate);
+    app.connect_activate(|app|
+    {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("wannabe Control Panel")
+            .build();
+
+        window.show();
+    );
 
     app.run();
 }
