9c3425b3c76c60e903f664834760cbbd9878b146
HelloWorld7894 <danpojhan@protonmail.com>
2022-11-04 16:34:33 +0100

    added face detection again

--- src/Mediapipe.py
+++ src/Mediapipe.py
@@ -1,11 +1,10 @@
 import cv2
 import mediapipe as mp
+mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
 
 #face_detection = mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
-pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+face_detection = mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
 
 # For webcam input:
 def DetectPose(image):
@@ -15,15 +14,12 @@ def DetectPose(image):
   # pass by reference.
   image.flags.writeable = False
   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-  results = pose.process(image)
+  results = face_detection.process(image)
 
-  # Draw the pose annotation on the image.
+  # Draw the face detection annotations on the image.
   image.flags.writeable = True
   image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-  mp_drawing.draw_landmarks(
-      image,
-      results.pose_landmarks,
-      mp_pose.POSE_CONNECTIONS,
-      landmark_drawing_spec=mp_drawing_styles
-      .get_default_pose_landmarks_style())
+  if results.detections:
+    for detection in results.detections:
+      mp_drawing.draw_detection(image, detection)
   return cv2.flip(image, 1), results
