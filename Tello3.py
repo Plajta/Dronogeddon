b3227bc2a30c0bd245f43fe4850545a661cf1ed0
HelloWorld7894 <danpojhan@protonmail.com>
2022-11-04 12:39:00 +0100

    created simple.py test

--- Tello3.py
+++ Tello3.py
@@ -33,14 +33,6 @@ def recv():
             print ('\nExit . . .\n')
             break
 
-
-print ('\r\n\r\nTello Python3 Demo.\r\n')
-
-print ('Tello: command takeoff land flip forward back left right \r\n       up down cw ccw speed speed?\r\n')
-
-print ('end -- quit demo.\r\n')
-
-
 #recvThread create
 recvThread = threading.Thread(target=recv)
 recvThread.start()
@@ -65,7 +57,3 @@ while True:
         print ('\n . . .\n')
         sock.close()
         break
-
-
-
-
---
+++ src/simple.py
@@ -0,0 +1,16 @@
+from djitellopy import Tello
+from time import sleep
+import logging
+Tello.LOGGER.setLevel(logging.DEBUG)
+
+tello = Tello()
+
+tello.connect(False)
+sleep(2)
+tello.takeoff()
+
+tello.move_left(100)
+tello.rotate_clockwise(90)
+tello.move_forward(100)
+
+tello.land()
