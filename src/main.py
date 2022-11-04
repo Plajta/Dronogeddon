1d1337ccae8231da8afd1cdbf2a2c9d88da351b3
ENGO150 <v.smejkal06@gmail.com>
2022-11-04 23:27:17 +0100

    nothing lmao

--- src/main.py
+++ src/main.py
@@ -7,12 +7,22 @@ window.geometry("500x500");
 def buttonPressed():
     Start()
 
+<<<<<<< HEAD
 button = tk.Button(
     text="Deploy",
     width=500,
     height=500,
     command=buttonPressed
 )
+=======
+button = tk.Button
+(
+    text = "Deploy",
+    width = 500,
+    height = 500,
+    command = buttonPressed
+);
+>>>>>>> parent of c02eec5 (removed spaces causing errors)
 
 button.pack()
 
