4195470fb6fd6e30ef6c34668b9d621a9b9be10c
Sles73 <usagisles@mail.com>
2022-11-04 14:37:51 +0100

    epilepsie

---
+++ src/img.py
@@ -0,0 +1,35 @@
+import time, cv2
+from threading import Thread
+from djitellopy import Tello
+from time import sleep
+tello = Tello()
+
+tello.connect()
+
+keepRecording = True
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+def videoRecorder():
+    height, width, _ = frame_read.frame.shape
+    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+    while keepRecording:
+        #video.write(frame_read.frame)
+        #time.sleep(1 / 30)
+        cv2.imshow("drone", frame_read.frame)
+        cv2.waitKey(100)
+        cv2.destroyAllWindows() 
+
+
+
+recorder = Thread(target=videoRecorder)
+recorder.start()
+
+tello.takeoff()
+tello.move_up(100)
+tello.rotate_counter_clockwise(360)
+tello.land()
+
+keepRecording = False
+recorder.join()
