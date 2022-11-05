c5c516ac378457c55afff0188f8a2dbbfb49e519
ENGO150 <v.smejkal06@gmail.com>
2022-11-05 08:09:36 +0100

    showing error-box when battery too low

--- src/main.py
+++ src/main.py
@@ -36,15 +36,15 @@ c = ctk.CTkButton(window, text="Stop", width=120, height=40, command=StopButton)
 
 ctk.CTkLabel(window, text="Tvoje jméno").grid(row=1)
 ctk.CTkLabel(window, text="email").grid(row=2)
-batteryLabel = ctk.CTkLabel(window, text=str(battery) + " %").grid(row=3)
-
-if battery <= 20:
-    batteryLabel.config(fg="red");
+ctk.CTkLabel(window, text=str(battery) + " %").grid(row=3)
 
 e1 = ctk.CTkEntry(window)
 e2 = ctk.CTkEntry(window)
 
 e1.grid(row=1, column=1)
 e2.grid(row=2, column=1)
+
+if battery <= 20:
+    messagebox.showwarning("warning","Warning")
 
 window.mainloop()
