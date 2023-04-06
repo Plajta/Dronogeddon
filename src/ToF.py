6c364debfee7c64c328e8d47806f3279baf80261
Sles73 <usagisles@mail.com>
2023-04-06 17:58:12 +0200

    Aleš konečně něco udělal

---
+++ src/ToF.py
@@ -0,0 +1,11 @@
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect(False)
+
+def mesurments():
+    responses = tello.send_read_command('EXT tof?').split()
+    mesurments = [int(responses[1]),int(responses[2]),int(responses[3])]
+    return mesurments
+
+    
