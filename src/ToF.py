128a35a95590ba66c47ae954c7fd3a59e391776c
Sles73 <usagisles@mail.com>
2023-04-27 18:09:09 +0200

    sensors in flight array mean function

--- src/ToF.py
+++ src/ToF.py
@@ -5,6 +5,11 @@ tello.connect(False)
 
 def mesurments():
     responses = tello.send_read_command('EXT tof?').split()
-    mesurments = [int(responses[1]),int(responses[2]),int(responses[3])]
-    return mesurments
+    if len(responses) < 4:
+        return mesurments()
+    else:
+        return [int(responses[1]),int(responses[2]),int(responses[3])]
+
+while True:
+    mesurments()
 
--- src/sensors_in_flight.py
+++ src/sensors_in_flight.py
@@ -1,35 +1,61 @@
+"""pole co dělá průměr posledních hodnot => počítat průměr 
+	dopředu odstraní chybné methody
+		zastavý provede další kontrolní měření - 5x"""
+
 from djitellopy import Tello
 import ToF as tf
 import time
 from threading import Thread
 
-border_low = 800
-border_high = border_low + 150
+margin_side_low = 800
+margin_side_high = 1500
+border_front = 950
 
-tello = Tello()
-tello.connect(False)
+mean = [[],[],[]]
+
+def distancemeter():
+    data = tf.mesurments()
+    output = data
+    for i in range(0,3):
+        mean[i].append(data[i])
+
+    if len(mean[0]) > 10:
+        for i in range(0,3):
+            mean[i].pop(0)
+    for i,j in enumerate(mean):
+        output.append(0)
+        for k in j:
+            output[i+3] += k
+        output[i+3] /= len(j)
+
+    return(output)
+
+
+
+
 
 start_time = time.time()
-log_time = time.strftime("%Y_%H_%M_%S", time.gmtime())
-log_pad = open(f"flight_log_{log_time}.txt", 'a')
+log_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.gmtime())
+log_pad = open(f"src/Flight_logs/flight_log_{log_time}.txt", 'w')
 log_pad.write(f"start of the program at {log_time}\n\n")
-log_pad.write(f"border low is: {border_low} || border high is {border_high} batery is\n\n")
+log_pad.write(f"side margin low is: {margin_side_low} || side mrgin high is {margin_side_high} || border front is {border_front}\n\n")
 
 def log(text=""):
     log_pad.write(f"{round(time.time()-start_time, 2)}: {text}\n")
 
+tello = Tello()
+tello.connect(False)
 
 log("tello takeoff")
 tello.takeoff()
+tello.move_up(100)
 
 pokracovac = True
 while pokracovac:
     data = tf.mesurments()
     distanceFront = data[0]
     log(data)
-    if distanceFront < border_low:
-        tello.send_rc_control(0,-20,0,0)
-    elif distanceFront > border_high:
+    if distanceFront > border_front:
         tello.send_rc_control(0,20,0,0)
     else:
         tello.send_rc_control(0,0,0,0)
@@ -46,24 +72,27 @@ while True:
     log("go against wall")
 
     while pokracovac:
-        data = tf.mesurments()
+        data = distancemeter()
+        log(data)
+
         distanceFront = data[0]
         distanceSide = data[2]
-        log(data)
         speedFront = 0 
         speedSide = 0
-        print(distanceSide)
-        if distanceSide < border_low:
+
+        if distanceSide < margin_side_low:
             speedSide = -20
-        elif distanceSide > border_high:
+
+        elif distanceSide > margin_side_high:
             speedSide = 20
+
         else:
             speedSide = 0
 
-        if distanceFront < border_low:
-            speedFront = -20
-        elif distanceFront > border_high:
+
+        if distanceFront > border_front:
             speedFront = 20
+
         else:
             speedFront = 0
             pokracovac = False
