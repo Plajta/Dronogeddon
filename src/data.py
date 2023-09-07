bcad7ccfc5ffba14bb88e0f86e9fa3fc0e13726e
HelloWorld7894 <danpojhan@protonmail.com>
2023-09-07 17:43:45 +0200

    added same base files

--- src/data.py
+++ src/data.py
@@ -1,2 +1,3 @@
 import numpy as np
 
+map_data = np.zeros((500, 500))
--- src/visual.py
+++ src/visual.py
@@ -1,4 +1,21 @@
 import cv2
 import numpy as np
+import data
+
+drone_last_pos = (0, 0)
+map = np.full((1000, 1000, 3), 255)
+
+def map_init():
+    data.map_data
+
+    #draw drone position (in center)
+    pass
 
 
+def update_map(drone_commands, curr_angle, dist):
+    pass
+
+
+
+if __name__ == "__main__":
+    map_init()
