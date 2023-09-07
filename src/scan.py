08ab4e22b4947d35a293cddf909bcbc4d94a8e1d
Sles Usagi <usagisles@mail.com>
2023-09-07 16:34:39 +0200

    lidar

---
+++ src/scan.py
@@ -0,0 +1,30 @@
+from djitellopy import Tello
+import ToF as tf
+import numpy as np
+
+tello = Tello()
+tello.connect(False)
+
+tello.takeoff()
+data = []
+
+# for i in range(8):
+#     deg = tello.get_yaw()
+#     dis = tf.mesurments()
+#     print(f"stupně: {deg[0]} vzdálenost {dis}")
+#     tello.rotate_clockwise(45)
+#     data.append([dis[0],deg])
+
+
+start = tello.get_yaw()
+tello.send_rc_control(0,0,0,10)
+while 1 == 1:
+    deg = tello.get_yaw()
+    dis = tf.mesurments()
+    print(f"stupně: {deg[0]} vzdálenost {dis}")
+    data.append([dis[0],deg])
+tello.send_rc_control(0,0,0,0)
+
+print(data)
+tello.land()
+
