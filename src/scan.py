b252accedd939daf663967895e55cbd1032fd56c
Sles Usagi <usagisles@mail.com>
2023-09-07 17:42:20 +0200

    scanning

--- src/scan.py
+++ src/scan.py
@@ -1,10 +1,12 @@
 from djitellopy import Tello
 import ToF as tf
 import numpy as np
+import time
 
 tello = Tello()
 tello.connect(False)
 
+tello.send_rc_control(0,0,0,0)
 tello.takeoff()
 data = []
 
@@ -17,14 +19,24 @@ data = []
 
 
 start = tello.get_yaw()
-tello.send_rc_control(0,0,0,10)
-while True:
+deg = start
+tello.send_rc_control(0,0,0,50)
+print("rotate")
+neg = False
+print(not(neg and deg >= start))
+print(start)
+while not(neg and deg >= start):
+    print(f"neg: {neg} deg >= start: {deg >= start} all: {neg and deg >= start}")
+    neg = deg < start
+    print(deg >= start)
+
     deg = tello.get_yaw()
     dis = tf.mesurments()
-    print(f"stupně: {deg[0]} vzdálenost {dis}")
+    print(f"vzdálenost: {dis[0]}  stupně: {deg}")
     data.append([dis[0],deg])
+    
 tello.send_rc_control(0,0,0,0)
-
+print("konec")
 print(data)
 tello.land()
 
