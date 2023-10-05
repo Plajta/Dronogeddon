0fd64f679ae60bda4aebe6e0b77f2696d58dfc96
Sles Usagi <usagisles@mail.com>
2023-10-05 17:37:00 +0200

    lepšejší

--- src/scan.py
+++ src/scan.py
@@ -7,6 +7,7 @@ import cv2
 from queue import Queue
 
 zastavovac = threading.Event()
+map_data = Queue()
 
 def visualisation():
     vis.map_init()
@@ -19,43 +20,46 @@ def visualisation():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-visual = Thread(target=visualisation)
+def scan():
+    visual = Thread(target=visualisation)
+    visual.start()
+    tello = Tello()
+    tello.connect(False)
 
-map_data = Queue()
-visual.start()
-tello = Tello()
-tello.connect(False)
-
-tello.send_rc_control(0,0,0,0)
-tello.takeoff()
-data = []
-
+    tello.send_rc_control(0,0,0,0)
+    tello.takeoff()
+    data = []
 
 
-start = tello.get_yaw()
-deg = start
-tello.send_rc_control(0,0,0,30)
-print("rotate")
-neg = False
-print(not(neg and deg >= start))
-print(start)
-while not(neg and deg >= start):
-    print(f"neg: {neg} deg >= start: {deg >= start} all: {neg and deg >= start}")
-    neg = deg < start
-    print(deg >= start)
 
-    deg = tello.get_yaw()
-    dis = tf.mesurments()
-    print(f"vzdálenost: {dis[0]}  stupně: {deg}")
-    
-    data.append([dis[0],deg])
-    map_data.put([dis[0],deg])
+    start = tello.get_yaw()
+    deg = start
+    tello.send_rc_control(0,0,0,30)
+    print("rotate")
+    neg = False
+    print(not(neg and deg >= start))
+    print(start)
+    while not(neg and deg >= start):
+        print(f"neg: {neg} deg >= start: {deg >= start} all: {neg and deg >= start}")
+        neg = deg < start
+        print(deg >= start)
 
-zastavovac.set()
-tello.send_rc_control(0,0,0,0)
-print("konec")
-print(data)
-tello.land()
+        deg = tello.get_yaw()
+        dis = tf.mesurments()
+        print(f"vzdálenost: {dis[0]}  stupně: {deg}")
+        
+        data.append([dis[0],deg])
+        map_data.put([dis[0],deg])
 
-while not cv2.waitKey(0) & 0xFF == ord('q'):
+    zastavovac.set()
+    tello.send_rc_control(0,0,0,0)
+    print("konec")
+    print(data)
+    tello.land()
+
+
+
+if __name__ == "__main__":
+    scan()
+    while not cv2.waitKey(0) & 0xFF == ord('q'):
         pass
