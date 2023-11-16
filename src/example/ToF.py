9a8ecba13d71e70f99a79fe3f15be06c1122c1bf
Sles Usagi <usagisles@mail.com>
2023-11-16 18:00:16 +0100

    translation

--- src/example/ToF.py
+++ src/example/ToF.py
@@ -6,13 +6,16 @@ tello.connect(False)
 
 
 def mesurments():
-    responses = tello.send_read_command('EXT tof?').split()
-    if len(responses) < 4:
+    try:
+        responses = tello.send_read_command('EXT tof?').split()
+        if len(responses) < 6:
+            return mesurments()
+        else:
+            return [int(responses[1]),int(responses[4]),int(responses[3]),int(responses[2]),int(responses[5])]
+    except Exception as e:
         return mesurments()
-    else:
-        return [int(responses[1]),int(responses[2]),int(responses[3])]
     
 
-#while True:
+while True:
     mesurments()
 
--- src/flight_controler.py
+++ src/flight_controler.py
@@ -1,8 +1,25 @@
 import time
 from djitellopy import Tello
+from enum import Enum
 
 tello = Tello()
 tello.connect(False)
+
+class Sensor(Enum):
+    F = 0
+    B = 3
+    L = 1
+    R = 2
+    D = 4
+
+class Direction(Enum):
+    F = 1
+    B = 1
+    L = 0
+    R = 0
+    D = 2
+
+margin = 5
 
 class FlightController():
     def __init__(self, drone_controller):
@@ -34,18 +51,48 @@ class FlightController():
         tello.send_rc_control(0,0,0,0)
         return data
     
-    def translation(axis,sensor):
-        pass
+    def translation(self,sensor,target_value):
+        direction = Direction[sensor].value
+        sensor = Sensor[sensor].value
+        distance = self.drone_controller.get_distance_data()[sensor]
+        speed = [0,0,0,0]
+        print(direction)
+        directionist = ((sensor%2)*2-1)*-1
+        stabilization = 0
+
+        while (target_value < distance - margin or target_value > distance + margin) and stabilization < 100:
+            if distance < target_value and speed[direction] != 40*directionist:
+                speed[direction] = -40*directionist
+                tello.send_rc_control(*speed)
+                print("flight to -1")
+                stabilization = 0
+            elif distance > target_value and speed[direction] != 60*directionist:
+                speed[direction] = 60*directionist
+                tello.send_rc_control(*speed)
+                print("flight to 1")
+                stabilization = 0
+            elif distance + margin > target_value > distance - margin:
+                stabilization += 1
+                tello.send_rc_control(0,0,0,0)
+
+            distance = self.drone_controller.get_distance_data()[sensor]
+            print(f"ano? {target_value < distance - margin or target_value > distance + margin} kolik: {distance} stabillization: {stabilization}")
+            time.sleep(0.1)
+        tello.send_rc_control(0,0,0,0)
+        print(distance)
 
     def flight_program(self):
         with self.drone_controller.telloLock:
             tello.takeoff()
+        self.current_work_thread = 1
 
         if self.current_work_thread == 0:
             print("start")
             scan_map = self.scan()
-        elif self.current_work_thread != 0:
-            pass 
+        elif self.current_work_thread == 1:
+            print("start translation")
+            self.translation("F",200)
+            print("end translation")
 
         with self.drone_controller.telloLock:
             tello.land()
