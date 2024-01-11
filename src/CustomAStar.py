75d79d5a7581b0119459f5e665e881171d251368
LosVocelos <janocelik@gmail.com>
2024-01-11 16:57:13 +0100

    Mby done AStar

--- src/CustomAStar.py
+++ src/CustomAStar.py
@@ -1,4 +1,5 @@
 from astar import AStar
+import math
 
 
 class CustomAStar(AStar):
@@ -6,16 +7,18 @@ class CustomAStar(AStar):
         self.nodes = nodes
 
     def neighbors(self, n):
-        for n1, d in self.nodes[n]:
+        for n1 in n.leading_to:
             yield n1
 
     def distance_between(self, n1, n2):
-        for n, d in self.nodes[n1]:
-            if n == n2:
-                return d
+        dx = n1.coord[0] - n2.coord[0]
+        dy = n1.coord[1] - n2.coord[1]
+        return math.sqrt(dx ** 2 + dy ** 2)
 
     def heuristic_cost_estimate(self, current, goal):
-        return 1
+        dx = current.coord[0] - goal.coord[0]
+        dy = current.coord[1] - goal.coord[1]
+        return math.sqrt(dx ** 2 + dy ** 2)
 
     def is_goal_reached(self, current, goal):
-        return current == goal
+        return current.id == goal.id
