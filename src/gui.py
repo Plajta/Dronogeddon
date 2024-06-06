4f4b6bd4e4b537e75048605f6e4c0b7a6bfa0f24
HelloWorld7894 <danpojhan@protonmail.com>
2024-06-06 16:37:00 +0200

    Tlačítka finally work, sick

--- src/gui.py
+++ src/gui.py
@@ -1,77 +1,93 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
-from multiprocessing import Queue, Process
+import threading
 import time
 
+#some class definitions
 class Window:
-    def __init__(self, win_name, loop_callback, dim=[700, 700]):
+    def __init__(self, win_name, dim=[700, 700]):
         self.win = tk.Tk()
         self.dimension = dim
         self.win.geometry(f"{dim[0]}x{dim[1]}")
-        
-        #setup & run process
-        self.process = Process(target=self.__run__, args=(loop_callback, ))
-        self.process.daemon = True
 
         #variables
-        self.scale_var = tk.DoubleVar() 
-        self.comm_queue = Queue()
+        self.scale_var = tk.DoubleVar()
+        self.thread_processes = []
+        self.win_closed = False
 
         #initial gui setup
         label = tk.Label(self.win, text=win_name)
         label.grid(row=0, column=0, columnspan=1)
 
-    #window functions for creation
-    def __run__(self, callback):
-        while True:
-            if self.comm_queue.empty():
-                callback()
-
-            if self.comm_queue.get() == "stop":
-                break
-
     def win_mainloop(self):
         self.win.protocol("WM_DELETE_WINDOW", self.close)
         self.win.mainloop()
-    
-    def start(self):
-        self.process.start()
+
+    def gui_function(self, target, delay):
+        """
+        Just a function to encapsulate target function so 
+        you don't have to retype window checking every time.
+        """
+
+        while not window.win_closed:
+            target()
+            if delay != None:
+                time.sleep(delay)
 
     def close(self):
         self.win_closed = True
+        self.stop_threads()
         self.win.destroy()
-        self.comm_queue.put("stop")
+
+    def set_thread(self, function, delay):
+        thread = threading.Thread(target=self.gui_function, args=(function, delay))
+        thread.daemon = True  # This will allow threads to exit when the main program exits
+        self.thread_processes.append(thread)
+
+    def run_threads(self):
+        for thread in self.thread_processes:
+            thread.start()
+
+    def stop_threads(self):
+        self.win_closed = True
+        for thread in self.thread_processes:
+            if thread.is_alive():
+                # Threads should exit cleanly as win_closed is checked in the loop
+                pass
 
     #functions for element creation
     def createTrackbar(self, coords, label, length, onchange, range=[0, 100]):
-        abs_coords = (coords[0], coords[1] + 1) 
+        abs_coords = (coords[0], coords[1] + 1)
 
         scale_text = tk.Label(self.win, text=label)
-        scale = tk.Scale(self.win, variable = self.scale_var, from_ = range[0], to = range[1], orient = tk.HORIZONTAL, length=length, command = onchange)    
+        scale = tk.Scale(self.win, variable=self.scale_var, from_=range[0], to=range[1], orient=tk.HORIZONTAL, length=length, command=onchange)
         scale_text.grid(row=abs_coords[1], column=abs_coords[0])
         scale.grid(row=abs_coords[1], column=abs_coords[0] + 1)
-    
+
     def createButton(self, coords, label, onclick):
         abs_coords = (coords[0], coords[1] + 1)
 
-        button = tk.Button(self.win, text = label, command = onclick)
+        button = tk.Button(self.win, text=label, command=onclick)
         button.grid(row=abs_coords[1], column=abs_coords[0])
-    
+
     def createLabel(self, coords, text):
-        abs_coords = (coords[0], coords[1] + 1) 
+        abs_coords = (coords[0], coords[1] + 1)
 
         label = tk.Label(self.win, text=text)
         label.grid(row=abs_coords[1], column=abs_coords[0])
 
-    def createViewbox(self, coords, img_source):
-        abs_coords = (coords[0], coords[1] + 1) 
+    def createViewbox(self, coords, img_source="blank"):
+        abs_coords = (coords[0], coords[1] + 1)
 
-        #convert cv2 to PIL
-        b,g,r = cv2.split(img_source)
-        img = cv2.merge((r,g,b))
-        img_pil = Image.fromarray(img)
-        img_pil = img_pil.resize((400, 400))
+        if img_source == "blank":
+            img_pil = Image.new("RGB", (400, 400))
+        else:
+            #convert cv2 to PIL
+            b, g, r = cv2.split(img_source)
+            img = cv2.merge((r, g, b))
+            img_pil = Image.fromarray(img)
+            img_pil = img_pil.resize((400, 400))
 
         img_gui = ImageTk.PhotoImage(img_pil)
 
@@ -85,8 +101,8 @@ class Window:
 
     def updateViewbox(self, viewbox, img_source):
         #convert cv2 to PIL
-        b,g,r = cv2.split(img_source)
-        img = cv2.merge((r,g,b))
+        b, g, r = cv2.split(img_source)
+        img = cv2.merge((r, g, b))
         img_pil = Image.fromarray(img)
         img_pil = img_pil.resize((400, 400))
 
@@ -96,32 +112,19 @@ class Window:
         viewbox.image = img_gui
 
 if __name__ == "__main__":
-    img = cv2.imread('image.png')
-    img2 = cv2.imread('image2.png')
-    print(img.shape)
+    
+    vid = cv2.VideoCapture(0) 
+    def get_camera(): #just an example function
+        ret, frame = vid.read()
+        window.updateViewbox(viewbox, frame)
 
-    def Test1(value):
-        print("Test1", value)
-
-    def Test2():
-        print("Test2")
-
-    def update():
-        global designator
-        time.sleep(1)
-        if designator == "lenna1":
-            window.updateViewbox(viewbox, img)
-            designator = "lenna2"
-        else:
-            window.updateViewbox(viewbox, img2)
-            designator = "lenna1"
-
-    window = Window("Testing window", update, dim=(1000, 1000))
-    global designator
-    designator = "lenna1"
-    viewbox = window.createViewbox((0, 0), img)
-    window.createTrackbar((0, 1), "Test", length=250, range=(0, 150), onchange=Test1)
+    window = Window("Testing window", dim=(1000, 1000))
+    viewbox = window.createViewbox((0, 0))
+    window.createTrackbar((0, 1), "Test", length=250, range=(0, 150), onchange=lambda x: None)
     window.createLabel((0, 2), "Test dva")
-    window.createButton((0, 3), "click me!", onclick=Test2)
-    window.start()
-    window.win_mainloop() #TODO: fix this in more civilised manner
+    window.createButton((0, 3), "click me!", onclick=lambda: None)
+
+    window.set_thread(get_camera, delay=None)
+    window.run_threads()
+
+    window.win_mainloop()
